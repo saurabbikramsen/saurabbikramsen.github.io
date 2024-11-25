d6a001f09837312dd2d391dd74fa2f43fbc2687b
saurabbikramsen <saurabbikramsen@gmail.com>
2024-11-25 22:30:56 +0545

    added the social platforms and mail

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { motion } from "framer-motion";
 import "./App.css";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaXTwitter,
+} from "react-icons/fa6";
 
 function App() {
   return (
     <>
-      <div className="pt-3 pb-5 pr-5 pl-5 bg-[#0A192F]">
+      <div className="pt-3 pb-5 pr-5 pl-5 h-screen bg-[#0A192F]">
         <div className=" flex items-center w-full h-16 md:h-20">
           <div className="flex pl-5">
             <svg
@@ -80,40 +86,94 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center h-screen w-full border-white">
+
+        <div className="flex gap-8 border-yellow-500 justify-center fixed md:w-[5%] left-6 bottom-0">
+          <div className="flex flex-col justify-end gap-5">
+            <div className="flex flex-col gap-5 items-center">
+              <a
+                href="https://instagram.com/saurab_bikram_sen"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 bg-[#64FFDA] rounded-full flex items-center justify-center hover:bg-[#4cd3b3] transition-colors"
+              >
+                <FaInstagram className="text-xl" />
+              </a>
+              <a
+                href="https://facebook.com/sbkrm.sen"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 bg-[#64FFDA] rounded-full flex items-center justify-center hover:bg-[#4cd3b3] transition-colors"
+              >
+                <FaFacebookF className="text-xl" />
+              </a>
+              <a
+                href="https://linkedin.com/in/saurab-sen-36653b250"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 bg-[#64FFDA] rounded-full flex items-center justify-center hover:bg-[#4cd3b3] transition-colors"
+              >
+                <FaLinkedinIn className="text-xl" />
+              </a>
+              <a
+                href="https://x.com/saurab_bkrm_sen"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="w-10 h-10 bg-[#64FFDA] rounded-full flex items-center justify-center hover:bg-[#4cd3b3] transition-colors"
+              >
+                <FaXTwitter className="text-xl" />
+              </a>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[1px] h-20 bg-white mt-4"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-8 border-yellow-500 justify-center right-5 fixed md:w-[5%] bottom-0">
+          <div className="flex flex-col justify-end gap-5">
+            <div className="flex flex-col items-center">
+              <a
+                href="mailto:saurabsen0@gmail.com"
+                className="text-white text-sm hover:text-[#4cd3b3] transition-colors vertical-text"
+                style={{ writingMode: "vertical-rl" }}
+              >
+                saurabbikramsen@gmail.com
+              </a>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[1px] h-20 bg-white mt-4"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-center w-full h-[75vh] border-white">
           <motion.div
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="flex justify-start border-red-500 h-full items-center"
+            className="flex justify-start border-red-500 items-center"
           >
-            <div className="flex gap-10 border-yellow-500 w-2/12 h-full">
-              <div className="flex flex-col justify-end gap-5">
-                <div className="w-10 h-10 bg-[#64FFDA] rounded-full"></div>
-                <div className="w-10 h-10 bg-[#64FFDA] rounded-full"></div>
-              </div>
-            </div>
-            <div className="flex flex-col p-5 gap-10 border-white w-7/12 ">
-              <div className="flex flex-col gap-5">
-                <span className="text-md text-[#64FFDA]">Hi, my name is</span>
-                <div className="flex flex-col gap-3">
-                  <h1 className="text-6xl font-bold text-gray-300">
-                    Saurab Bikram Sen.
-                  </h1>
-                  <h1 className="text-6xl font-bold text-[#818bab]">
-                    I build scalable backend systems.
-                  </h1>
+            <div className="flex flex-col p-5 gap-10 w-full ml-[12%] border-white ">
+              <div className="flex flex-col gap-10 w-9/12 border-red-500">
+                <div className="flex flex-col gap-5">
+                  <span className="text-sm text-[#64FFDA]">Hi, my name is</span>
+                  <div className="flex flex-col gap-3">
+                    <h1 className="text-6xl font-bold text-gray-300">
+                      Saurab Bikram Sen.
+                    </h1>
+                    <h1 className="text-6xl font-bold text-[#818bab]">
+                      I build scalable backend systems.
+                    </h1>
+                  </div>
                 </div>
+                <p className="text-sm text-gray-400 w-2/3">
+                  I’m a backend developer passionate about designing and
+                  implementing robust server-side solutions. Currently, I focus
+                  on creating high-performance, scalable architectures and APIs
+                  that power seamless, data-driven applications.
+                </p>
+                <button className="text-sm px-4 py-4 w-2/6 border border-[#64FFDA] text-[#64FFDA] rounded hover:bg-[#64FFDA]/10 transition-colors">
+                  check out my cv!!
+                </button>
               </div>
-              <p className="text-md text-gray-400 w-2/3">
-                I’m a backend developer passionate about designing and
-                implementing robust server-side solutions. Currently, I focus on
-                creating high-performance, scalable architectures and APIs that
-                power seamless, data-driven applications.
-              </p>
-              <button className="text-md px-4 py-4 w-2/6 border border-[#64FFDA] text-[#64FFDA] rounded hover:bg-[#64FFDA]/10 transition-colors">
-                check out my cv!!
-              </button>
             </div>
           </motion.div>
         </div>
